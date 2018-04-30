e5f7d306494f3f73aa0bc747585b3fc8d4280619
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-04-30 11:41:28 -0700

    template

--- app/javascript/UserList/UserList.jsx
+++ app/javascript/UserList/UserList.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { GlobalHeader, PageHeader, Cards } from 'react-wood-duck';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
-class Cognito extends React.Component {
+export class UserList extends React.Component {
+  displayList = () => {
+    return (
+      <div className="col-md-12">
+        <br />
+        <BootstrapTable>
+          <TableHeaderColumn dataField="Username" dataSort>
+            UserName/Acct #
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Role" isKey hidden dataSort>
+            Assigned Role
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Status" dataSort>
+            Status
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Updated" dataSort>
+            Updated
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Created" dataSort>
+            Created
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="Actions" dataSort>
+            Actions
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  };
   render() {
-    return <h1>{this.props.name}</h1>;
+    return (
+      <div>
+        <GlobalHeader />
+        <PageHeader pageTitle="Admin User Name" button="" />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Cards
+                cardHeaderText="County Users List"
+                cardActionButtons={false}
+              >
+                {this.displayList()}
+              </Cards>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-Cognito.propTypes = {
-  name: PropTypes.string,
-};
-Cognito.defaultProps = {
-  name: 'Hello Cognito App User!',
-};
-export default Cognito;
+
+export default UserList;
+
+UserList.propTypes = {};
--- app/javascript/UserList/UserList.test.js
+++ app/javascript/UserList/UserList.test.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Cognito from './UserList.jsx';
+import UserList from './UserList.jsx';
 
 describe('Cognito', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<Cognito />);
+    wrapper = shallow(<UserList />);
   });
 
-  it('verify the div tag', () => {
-    expect(wrapper.find('h1').length).toEqual(1);
+  it('renders a Cards', () => {
+    expect(wrapper.find('Cards').length).toBeGreaterThan(0);
   });
 
-  it('verify the props', () => {
-    expect(wrapper.find('h1').props().children).toEqual(
-      'Hello Cognito App User!'
-    );
+  it('#displayList function defined', () => {
+    const instance = wrapper.instance();
+    expect(instance.displayList()).toBeDefined();
+  });
+
+  describe('#displayList output', () => {
+    it('displays table', () => {
+      expect(wrapper.find('BootstrapTable').length).toBe(1);
+      expect(wrapper.find('TableHeaderColumn').length).toBe(6);
+    });
   });
 });
