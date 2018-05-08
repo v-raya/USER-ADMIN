1d567a0a2cdfd9c88dcf03813f30e1bc3bece0fe
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-05-08 15:43:47 -0700

    rspec test

---
+++ spec/controllers/user_details_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe UserDetailsController do
+  describe '#index' do
+    it 'has route' do
+      expect(get: 'user_details/index').to route_to(controller: 'user_details', action: 'index')
+    end
+  end
+end
