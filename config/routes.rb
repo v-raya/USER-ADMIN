4b4c0fbbd172e81d85925de430d56173b3da4158
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-05-09 17:57:38 -0700

    COG-60 Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
-  get 'user_list', to:'user_details#index'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  get 'user_details', to: 'user_details#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: 'json' } do
     resources :user_list, only: [:index] do
