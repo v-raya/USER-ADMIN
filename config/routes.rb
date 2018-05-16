52c721d03692882ee64db62899a5a31c1152bb70
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-05-16 13:24:37 -0700

    changed routing path to take id for link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
-  get 'user_details', to: 'user_details#index'
+  get 'user_details/:id', to: 'user_details#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: 'json' } do
     resources :user_list, only: [:index] do
--- spec/controllers/user_details_controller_spec.rb
+++ spec/controllers/user_details_controller_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 describe UserDetailsController do
   describe '#index' do
     it 'has route' do
-      expect(get: 'user_details').to route_to(controller: 'user_details', action: 'index')
+      expect(get: 'user_details/id')
+        .to route_to(controller: 'user_details', id: 'id', action: 'index')
     end
   end
 end
