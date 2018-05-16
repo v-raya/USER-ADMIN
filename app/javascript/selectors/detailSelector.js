70803e7489461386101cab60619a0cf69b4ea272
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-05-16 11:56:23 -0700

    COG-62 Remove console.log lines

--- app/javascript/selectors/detailSelector.js
+++ app/javascript/selectors/detailSelector.js
@@ -1,5 +1,4 @@
 export const selectDetailRecords = state => {
-  console.log('usersDetails state', state);
   const usersObject = state.fetchDetails ? state.fetchDetails.details : null;
   return usersObject ? usersObject.records : {};
 };
--- app/javascript/selectors/userListSelector.js
+++ app/javascript/selectors/userListSelector.js
@@ -1,5 +1,4 @@
 export const selectUserRecords = state => {
-  console.log('usersList State', state);
   const usersObject = state.fetchUserList ? state.fetchUserList.userList : null;
   return usersObject ? usersObject.records : [];
 };
