079173dbcc396309531f23fd6905c288c8013257
Kevin Fries <kelfink@yahoo.com>
2018-05-22 09:57:22 -0700

    Try Nesh's fix for asset resolution using config.assets.prefix in application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module CountyAdmin
     
     config.micro_services = config_for(:micro_services)
     config.relative_url_root = ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
+    config.assets.prefix = "#{ENV['RAILS_RELATIVE_URL_ROOT']}/assets"
   end
 end
