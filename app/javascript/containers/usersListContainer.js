385dca0f4990f47bea792467ad596f9bc2f5a9e7
Kevin Fries <kelfink@yahoo.com>
2018-05-23 09:21:15 -0700

    WIP adding calls to fetch account in the front-end

--- app/javascript/containers/usersListContainer.js
+++ app/javascript/containers/usersListContainer.js
@@ -3,6 +3,30 @@ import UsersList from '../views/usersList/UsersList.jsx';
 import * as actionTypes from '../actions/actionTypes';
 import { selectUserRecords } from '../selectors/userListSelector';
 
+const fetchUser = () => {
+  console.log('fetching account');
+  this.setState({ XHRStatus: 'waiting' });
+  return AccountService.fetchCurrent()
+    .then(this.onFetchSuccess)
+    .catch(() => this.setState({ XHRStatus: 'error' }));
+};
+
+const onFetchSuccess = staffPerson => {
+  console.log('on fetch succeee', staffPerson);
+  this.setState({
+    XHRStatus: 'ready',
+    user: {
+      user: staffPerson,
+    },
+  });
+  console.log('Got the account', staffPerson);
+};
+
+const componentDidMount = () => {
+  //this.fetchUser();
+  console.log('componenr did mount');
+};
+
 const mapStateToProps = state => {
   return {
     userList: selectUserRecords(state),
