4e446d061e287485dfbe16899bb759fa71d30cbb
Kevin Fries <kelfink@yahoo.com>
2018-05-24 22:53:25 -0700

    Enable acceptance test

--- spec/acceptance/support/login_helper.rb
+++ spec/acceptance/support/login_helper.rb
@@ -2,10 +2,12 @@
 
 module LoginHelper
   def login(login_config = default_json)
-    visit '/'
-    return unless ENV.fetch('CASE_AUTHORIZATION_ENABLED', false)
+    visit ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
+    puts "visited: landed on  " + current_url
+    return unless ENV.fetch('COUNTY_AUTHORIZATION_ENABLED', false)
     fill_in 'Authorization JSON', with: JSON.generate(login_config)
     click_button 'Sign In'
+    puts "signed in: " + current_url
   end
 
   private
@@ -17,7 +19,8 @@ module LoginHelper
       'roles': ['Supervisor'],
       'county_code': '56',
       'county_cws_code': '1123',
-      'county_name': 'Ventura',
+      'county_name': 'Placer',
+      'roles': [ 'CWS-admin', 'Supervisor' ],
       'privileges': [
         'CWS Case Management System',
         'Resource Management',
