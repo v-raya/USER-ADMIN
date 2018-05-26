5f53747f758e8c6fc0d764aa330f7ca8cd60b97f
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-05-26 16:11:19 -0700

    Fix Bug to display specific users

--- app/javascript/sagas/getDetailsSaga.js
+++ app/javascript/sagas/getDetailsSaga.js
@@ -2,12 +2,13 @@ import UserService from '../_services/users';
 import * as actionTypes from '../actions/actionTypes';
 import { takeLatest, call, put } from 'redux-saga/effects';
 
-const id = 'id';
 // worker saga: makes the api call when watcher saga sees the action
 export function* getDetails() {
   try {
+    const id = getUserId(currentPathname());
+    // console.log(id);
     const details = yield call(UserService.fetchUserDetails, id);
-
+    console.log(details);
     // dispatch a success action to the store with the new users
     yield put({
       type: actionTypes.FETCH_DETAILS_API_CALL_SUCCESS,
@@ -21,6 +22,16 @@ export function* getDetails() {
       error,
     });
   }
+}
+
+export function currentPathname() {
+  return window.location.pathname;
+}
+
+export function getUserId(pathname) {
+  const pathArray = pathname.split('/');
+  const id = pathArray[pathArray.length - 1];
+  return id;
 }
 
 // watcher saga: watches for actions dispatched to the store, starts worker saga
--- app/javascript/sagas/getDetailsSaga.test.js
+++ app/javascript/sagas/getDetailsSaga.test.js
@@ -1,11 +1,16 @@
 import UserService from '../_services/users';
 
-import { detailsSaga, getDetails } from './getDetailsSaga';
+import {
+  detailsSaga,
+  getDetails,
+  getUserId,
+  currentPathname,
+} from './getDetailsSaga';
 import * as actionTypes from '../actions/actionTypes';
 import { takeLatest, call, put } from 'redux-saga/effects';
 
-const id = 'id';
-UserService.fetchUserDetails(id);
+// const id = 'id';
+// UserService.fetchUserDetails(id);
 
 describe('sagas', () => {
   it('starts the worker fetch saga', () => {
@@ -23,8 +28,9 @@ describe('sagas', () => {
     describe('when successful', () => {
       it('executes the happy-path saga', () => {
         const gen = getDetails();
+        const userID = getUserId(currentPathname());
         expect(gen.next().value).toEqual(
-          call(UserService.fetchUserDetails, id)
+          call(UserService.fetchUserDetails, userID)
         );
         expect(gen.next({ id: 12, name: 'username' }).value).toEqual(
           put({
@@ -39,8 +45,9 @@ describe('sagas', () => {
     describe('when failures come back from the fetch', () => {
       it('handles the error', () => {
         const gen = getDetails();
+        const userID = getUserId(currentPathname());
         expect(gen.next().value).toEqual(
-          call(UserService.fetchUserDetails, id)
+          call(UserService.fetchUserDetails, userID)
         );
         expect(gen.throw('I have made a huge mistake').value).toEqual(
           put({
@@ -49,6 +56,14 @@ describe('sagas', () => {
           })
         );
         expect(gen.next().done).toBe(true);
+      });
+    });
+
+    describe('#getUserID', () => {
+      it('gets the userId ', () => {
+        let pathname = '/user_list/9099';
+        const gen = getUserId(pathname);
+        expect(gen).toEqual('9099');
       });
     });
   });
