89de660e0aedc2f3d82c0eeb06c96d7a2e6e4115
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-05-29 12:05:43 -0700

    handling edit mode

--- app/javascript/views/userDetail/UserDetail.jsx
+++ app/javascript/views/userDetail/UserDetail.jsx
@@ -1,101 +1,155 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { GlobalHeader, PageHeader, Cards, Link } from 'react-wood-duck';
+import { GlobalHeader, PageHeader, Link, Alert } from 'react-wood-duck';
 import ShowField from '../../common/ShowField';
+import Cards from '../../common/Card';
+import UserDetailEdit from '../userDetail/UserDetailEdit';
 
 /* eslint camelcase: 0 */
+export default class UserDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEdit: false,
+      alert: false,
+      enableSave: true,
+    };
+  }
 
-const UserDetail = ({
-  details,
-  dashboardUrl,
-  userListUrl,
-  userListClickHandler,
-  dashboardClickHandler,
-}) => (
-  <div>
-    <GlobalHeader profileName="County CWDS-Admin" profileId="profile.id" />
-    <PageHeader pageTitle="User Profile" button="" />
-    <div className="container">
-      <div className="col-md-12">
-        Back to:{' '}
-        <Link
-          text="Dashboard"
-          href={dashboardUrl}
-          clickHandler={dashboardClickHandler}
-        />
-        &nbsp;&gt;&nbsp;
-        <Link
-          text="User List"
-          href={userListUrl}
-          clickHandler={userListClickHandler}
-        />
-      </div>
+  onStatusChange = value => {
+    this.setState({ enableSave: false });
+  };
 
-      <div className="row">
-        <div className="col-md-12">
-          <Cards
-            cardHeaderText={`Manage Users: ${details.county_name}`}
-            cardHeaderButton={true}
-          >
+  onRoleChange = value => {
+    this.setState({ enableSave: false });
+  };
+
+  render() {
+    const {
+      details,
+      dashboardUrl,
+      userListUrl,
+      userListClickHandler,
+      dashboardClickHandler,
+    } = this.props;
+
+    return (
+      <div>
+        <div>
+          <GlobalHeader
+            profileName="County CWDS-Admin"
+            profileId="profile.id"
+          />
+          <PageHeader pageTitle="User Profile" button="" />
+          <div className="container">
             <div className="col-md-12">
-              <div className="row">
-                <div className="col-md-3">
-                  <ShowField label="Full Name">
-                    {details.last_name},
-                    {details.first_name}
-                    {details.middle_name}
-                  </ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="Office Name">{details.office}</ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="CWS Login">{details.racfid}</ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="Last Login">
-                    {details.last_login_date_time}
-                  </ShowField>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-3">
-                  <ShowField label="Email">{details.email}</ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="Office Phone Number">
-                    <span>{details.phone_number}</span>
-                  </ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="Start Date">
-                    {' '}
-                    {details.start_date}
-                  </ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="End Date">{details.end_date}</ShowField>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-3">
-                  <ShowField label="Status">
-                    {String(details.enabled)}
-                  </ShowField>
-                </div>
-                <div className="col-md-3">
-                  <ShowField label="Assigned Roles">
-                    {details.permissions}
-                  </ShowField>
-                </div>
-              </div>
+              {this.state.alert && (
+                <Alert alertClassName="success" faIcon="fa-check-circle">
+                  {'Your changes have been made successfuly'}
+                </Alert>
+              )}
+              Back to:{' '}
+              <Link
+                text="Dashboard"
+                href={dashboardUrl}
+                clickHandler={dashboardClickHandler}
+              />
+              &nbsp;&gt;&nbsp;
+              <Link
+                text="User List"
+                href={userListUrl}
+                clickHandler={userListClickHandler}
+              />
             </div>
-          </Cards>
+            {this.state.isEdit ? (
+              <div>
+                {' '}
+                <UserDetailEdit
+                  details={details}
+                  onCancel={() => this.setState({ isEdit: false })}
+                  onSave={() => this.setState({ isEdit: false, alert: true })}
+                  onStatusChange={this.onStatusChange}
+                  onRoleChange={this.onRoleChange}
+                  enableSave={this.state.enableSave}
+                />{' '}
+              </div>
+            ) : (
+              <div className="row">
+                <div className="col-md-12">
+                  <Cards
+                    cardHeaderText={`Manage Users: ${details.county_name}`}
+                    cardHeaderButton={true}
+                    onEdit={() => this.setState({ isEdit: true })}
+                  >
+                    <div className="col-md-12">
+                      <div className="row">
+                        <div className="col-md-3">
+                          <ShowField label="Full Name">
+                            {details.last_name},
+                            {details.first_name}
+                            {details.middle_name}
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="Office Name">
+                            {details.office}
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="CWS Login">
+                            {details.RACFID}
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="Last Login">
+                            {details.last_login_date_time}
+                          </ShowField>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-3">
+                          <ShowField label="Email">{details.email}</ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="Office Phone Number">
+                            <span>{details.phone_number}</span>
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="Start Date">
+                            {' '}
+                            {details.start_date}
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="End Date">
+                            {details.end_date}
+                          </ShowField>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-3">
+                          <ShowField label="Status">
+                            {String(details.enabled)}
+                          </ShowField>
+                        </div>
+                        <div className="col-md-3">
+                          <ShowField label="Assigned Roles">
+                            {details.permissions}
+                          </ShowField>
+                        </div>
+                      </div>
+                    </div>
+                  </Cards>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 UserDetail.propTypes = {
   details: PropTypes.object,
@@ -111,5 +165,3 @@ UserDetail.defaultProps = {
   userListClickHandler: () => {},
   dashboardClickHandler: () => {},
 };
-
-export default UserDetail;
