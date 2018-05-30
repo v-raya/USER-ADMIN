8f921e69cc2ab24072d145602e11cc4bdbcbc1ad
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-05-29 21:34:16 -0700

    COG-40 Update details saga to display specific user details

--- app/javascript/sagas/getDetailsSaga.js
+++ app/javascript/sagas/getDetailsSaga.js
@@ -5,11 +5,9 @@ import { takeLatest, call, put } from 'redux-saga/effects';
 // worker saga: makes the api call when watcher saga sees the action
 export function* getDetails() {
   try {
-    const id = getUserId();
+    const id = getUserId(currentPathname());
     const details = yield call(UserService.fetchUserDetails, id);
-
     // dispatch a success action to the store with the new users
-
     yield put({
       type: actionTypes.FETCH_DETAILS_API_CALL_SUCCESS,
       details,
@@ -24,8 +22,12 @@ export function* getDetails() {
   }
 }
 
-export function getUserId() {
-  const pathArray = window.location.pathname.split('/');
+export function currentPathname() {
+  return window.location.pathname;
+}
+
+export function getUserId(pathname) {
+  const pathArray = pathname.split('/');
   const id = pathArray[pathArray.length - 1];
   return id;
 }
