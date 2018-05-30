a6e790e278759cfa451727667648b0753e652285
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-05-30 10:12:00 -0700

    COG-40 Remove Commented Lines

--- app/controllers/api/user_list_controller.rb
+++ app/controllers/api/user_list_controller.rb
@@ -2,11 +2,6 @@
 
 module Api
   class UserListController < ActionController::API
-    # def index
-    #   users = Users::UserRepository.new.get_users(session[:token])
-    #   render json: users
-    # end
-
     def index
       users = Users::UserRepository.new.get_users(params[:last_name], session[:token])
       render json: users
