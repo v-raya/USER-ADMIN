383ba6fddd051534efff2f1e7a5197182f26cab5
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-06-04 13:56:06 -0700

    added permissions reducer

---
+++ app/javascript/reducers/permissionsReducer.test.js
@@ -0,0 +1,60 @@
+import fetchPermissions from './permissionsReducers';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('reducer', () => {
+  it('handles FETCH_PERMISSIONS_API_CALL_REQUEST', () => {
+    const requestAction = {
+      type: actionTypes.FETCH_PERMISSIONS_API_CALL_REQUEST,
+    };
+    const state = { permissions: null, fetching: false };
+    expect(fetchPermissions(state, requestAction)).toEqual({
+      fetching: true,
+      permissions: null,
+      error: null,
+    });
+  });
+
+  it('handles FETCH_PERMISSIONS_API_CALL_SUCCESS', () => {
+    const responseAction = {
+      type: actionTypes.FETCH_PERMISSIONS_API_CALL_SUCCESS,
+      permissions: ['Hello', 'Bye'],
+    };
+    const state = { permissions: null, fetching: true, error: null };
+
+    expect(fetchPermissions(state, responseAction)).toEqual({
+      fetching: false,
+      permissions: {
+        XHRStatus: 'ready',
+        permissions: ['Hello', 'Bye'],
+      },
+      error: null,
+    });
+  });
+
+  it('handles FETCH_PERMISSIONS_API_CALL_FAILURE', () => {
+    const failureAction = {
+      type: actionTypes.FETCH_PERMISSIONS_API_CALL_FAILURE,
+      permissions: null,
+      error: 'error happened',
+    };
+    const state = { permissions: null, fetching: true, error: null };
+    expect(fetchPermissions(state, failureAction)).toEqual({
+      fetching: false,
+      permissions: null,
+      error: 'error happened',
+    });
+  });
+
+  it('handles unexpected actions by ignoring them', () => {
+    const randomAction = {
+      type: 'unknownAction',
+      foreignObject: [],
+    };
+    const state = { permissions: null, fetching: true, error: null };
+    expect(fetchPermissions(state, randomAction)).toEqual({
+      permissions: null,
+      fetching: true,
+      error: null,
+    });
+  });
+});
---
+++ app/javascript/reducers/permissionsReducers.js
@@ -0,0 +1,35 @@
+import * as actionTypes from '../actions/actionTypes';
+
+function fetchPermissions(
+  state = { permissions: null, fetching: false },
+  action
+) {
+  switch (action.type) {
+    case actionTypes.FETCH_PERMISSIONS_API_CALL_REQUEST:
+      return { ...state, fetching: true, error: null };
+
+    case actionTypes.FETCH_PERMISSIONS_API_CALL_SUCCESS:
+      const permissionsList = {
+        XHRStatus: 'ready',
+        permissions: [...action.permissions],
+      };
+      return {
+        ...state,
+        fetching: false,
+        permissions: permissionsList,
+        error: null,
+      };
+
+    case actionTypes.FETCH_PERMISSIONS_API_CALL_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        permissions: null,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
+
+export default fetchPermissions;
