48bf2fc6a7adb54998f4a7a0c3c34869c9c3d0ee
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-06-04 10:16:43 -0700

    added service to get list of permissions

---
+++ app/controllers/api/permissions_list_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  class PermissionsListController < ActionController::API
+    def index
+      token = session[:token]
+      permissions = Users::UserRepository.new.get_permissions_list(token)
+      render json: permissions
+    end
+  end
+end
+
+  
