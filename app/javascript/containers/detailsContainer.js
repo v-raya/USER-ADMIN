526c39c5d280debb6e82b5cdba56cc43f31d665a
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-06-04 13:48:51 -0700

    fetching permission actions

--- app/javascript/containers/detailsContainer.js
+++ app/javascript/containers/detailsContainer.js
@@ -1,16 +1,20 @@
 import { connect } from 'react-redux';
 import Details from '../views/userDetail/UserDetail.jsx';
-import * as actionTypes from '../actions/actionTypes';
-import { selectDetailRecords } from '../selectors/detailSelector';
+// import * as actionTypes from '../actions/actionTypes';
+import {
+  selectDetailRecords,
+  permissionsList,
+} from '../selectors/detailSelector';
 
 const mapStateToProps = state => {
   return {
     userList: selectDetailRecords(state),
+    permissionsList: permissionsList(state),
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  dispatch({ type: actionTypes.FETCH_DETAILS_API_CALL_REQUEST });
+  // dispatch({ type: actionTypes.FETCH_DETAILS_API_CALL_REQUEST });
   return {};
 };
 
