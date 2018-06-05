2073f6871e7c8ca8383191d81573065b69e9eccf
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-06-05 14:19:04 -0700

    removed hardcoded options

--- app/javascript/_constants/userDetailConstants.js
+++ app/javascript/_constants/userDetailConstants.js
@@ -2,11 +2,3 @@ export const STATUS = [
   { value: 'true', label: 'True' },
   { value: 'false', label: 'False' },
 ];
-
-export const ROLES = [
-  { value: 'Facility-search-rollout', label: 'Facility-search-rollout' },
-  { value: 'Hotline-rollout', label: 'Hotline-rollout' },
-  { value: 'RFA-rollout', label: 'RFA-rollout' },
-  { value: 'Snapshot-rollout', label: 'Snapshot-rollout' },
-  { value: 'development-not-in-use', label: 'development-not-in-use' },
-];
