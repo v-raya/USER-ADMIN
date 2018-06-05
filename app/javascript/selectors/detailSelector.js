d55bc621064f7d916651141b8d35d075cd6da3bc
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-06-05 16:47:08 -0700

    Map to active and inactive based on bool values

--- app/javascript/selectors/detailSelector.js
+++ app/javascript/selectors/detailSelector.js
@@ -1,4 +1,14 @@
 export const selectDetailRecords = state => {
   const usersObject = state.fetchDetails ? state.fetchDetails.details : null;
-  return usersObject ? usersObject.records : {};
+  const userDetails = usersObject ? usersObject.records : {};
+  console.log('status ', userDetails.enabled);
+  if (userDetails.enabled !== true) {
+    userDetails.enabled = 'Active';
+  } else {
+    userDetails.enabled = 'Inactive';
+  }
+  // const userStatus = userDetails.enabled
+  //   ? userDetails.enabled === true
+  //   : 'Active';
+  return userDetails;
 };
