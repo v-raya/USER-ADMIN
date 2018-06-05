d3cc832a49e2c6cec3e576bf1b1764bd4647128c
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-06-05 14:20:12 -0700

    fetching permissionsActions

--- app/javascript/detailsPage/detailsPage.js
+++ app/javascript/detailsPage/detailsPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import UserService from '../_services/users/user.service';
 import DetailsContainer from '../containers/detailsContainer';
 import { fetchDetailsActions } from '../actions/detailActions';
+import { fetchPermissionsActions } from '../actions/permissionsActions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { selectDetailRecords } from '../selectors/detailSelector';
@@ -9,7 +11,9 @@ import { selectDetailRecords } from '../selectors/detailSelector';
 class DetailsPage extends React.Component {
   componentDidMount() {
     this.props.actions.fetchDetailsActions();
+    this.props.actions.fetchPermissionsActions();
   }
+
   render() {
     return <DetailsContainer details={this.props.details} />;
   }
@@ -27,7 +31,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  const actions = { fetchDetailsActions };
+  const actions = { fetchDetailsActions, fetchPermissionsActions };
   return {
     actions: bindActionCreators(actions, dispatch),
   };
