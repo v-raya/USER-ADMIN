29e87a9887be134fbc94d835f2dbabebcb871a97
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-06-06 13:07:22 -0700

    COG-182 Convert true of false to active/inactive in user details

--- app/javascript/selectors/detailSelector.js
+++ app/javascript/selectors/detailSelector.js
@@ -1,14 +1,10 @@
 export const selectDetailRecords = state => {
   const usersObject = state.fetchDetails ? state.fetchDetails.details : null;
   const userDetails = usersObject ? usersObject.records : {};
-  console.log('status ', userDetails.enabled);
-  if (userDetails.enabled !== true) {
-    userDetails.enabled = 'Active';
-  } else {
-    userDetails.enabled = 'Inactive';
-  }
-  // const userStatus = userDetails.enabled
-  //   ? userDetails.enabled === true
-  //   : 'Active';
+  userDetails.enabled === undefined
+    ? (userDetails.enabled = '')
+    : userDetails.enabled
+      ? (userDetails.enabled = 'Active')
+      : (userDetails.enabled = 'Inavtive');
   return userDetails;
 };
