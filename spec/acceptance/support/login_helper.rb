edc2a65e141f3d84a2625fc4d3260205bdebc64e
Kevin Fries <kelfink@yahoo.com>
2018-06-14 11:16:33 -0700

    Pre-find the auth json field

--- spec/acceptance/support/login_helper.rb
+++ spec/acceptance/support/login_helper.rb
@@ -7,7 +7,7 @@ module LoginHelper
     puts "login_config #{login_config}"
     return unless ENV.fetch('COUNTY_AUTHORIZATION_ENABLED', false)
     check_status login_config
-    field_labeled("Authorization JSON", {disabled: false, wait: 5})
+    field_labeled('Authorization JSON', disabled: false, wait: 5)
     fill_in 'username', with: login_json
     click_button 'Sign In'
     puts "signed in: #{current_url}"
