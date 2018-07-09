89234a7bb97aadc47817c1975f860d346dfc1057
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-07-09 15:12:36 -0700

    renders id from create user

--- app/javascript/selectors/createUserSelector.js
+++ app/javascript/selectors/createUserSelector.js
@@ -1,5 +1,4 @@
 export const selectCreateUserRecords = state => {
   const usersObject = state.createUser ? state.createUser.createNewUser : '';
-  console.log('SELECTORS RESPONSE ', usersObject);
   return usersObject;
 };
--- app/javascript/views/addUser/AddUser.jsx
+++ app/javascript/views/addUser/AddUser.jsx
@@ -3,39 +3,30 @@ import PropTypes from 'prop-types';
 import AddVerifiedUser from './AddVerifiedUser';
 import VerifyUser from './VerifyUser';
 import { Link, PageHeader } from 'react-wood-duck';
-import UserDetail from './addUserDetails';
+import AddUserDetail from '../../containers/addUserDetailsContainer';
 
-// import UserService from '../../_services/users';
 /* eslint camelcase: 0 */
 export default class AddUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      addUser: props.addUser,
-      add: false,
       verify: false,
       confirm: false,
       racfid: '',
       email: '',
       disableActionBtn: true,
+      addUser: props.addUser,
       verifyNewUserDetails: props.verifyNewUserDetails,
-      createUserDetails: props.createUserDetails,
       details: props.details,
-      permissionsList: props.permissionsList,
-
-      // addUsers: {
-      //   XHRStatus: '',
-      //   records: undefined,
-      // },
     };
   }
 
   componentWillReceiveProps(nextProps) {
     let verifyNewUserDetails = nextProps.verifyNewUserDetails;
-    let createUserDetails = nextProps.createUserDetails;
+    let id = nextProps.id;
+    let permissionRoles = nextProps.permissionRoles;
     let details = nextProps.details;
-    this.setState({ verifyNewUserDetails, createUserDetails, details });
-    // this.fetchIndianAncestryData();
+    this.setState({ verifyNewUserDetails, id, details, permissionRoles });
   }
 
   handleEmail = event => {
@@ -46,29 +37,6 @@ export default class AddUser extends Component {
       this.setState({ disableActionBtn: true });
     }
   };
-
-  // fetchIndianAncestryData = () => {
-  // const newUser = {
-  //   email: 'kevinfs@gmail.com',
-  //   first_name: 'User1asdfasd',
-  //   last_name: 'Two1asfdasdf',
-  //   county_name: 'Madera',
-  //   racfid: 'POPSOPS',
-  //   enabled: true,
-  //   status: 'FORCE_CHANGE_PASSWORD',
-  // };
-  //   this.setState({ addUsers: { XHRStatus: 'waiting' } });
-  //   return UserService.createUser(newUser)
-  //     .then(addUsers => {
-  //       this.setState({
-  //         addUsers: {
-  //           XHRStatus: 'ready',
-  //           records: addUsers,
-  //         },
-  //       });
-  //     })
-  //     .catch(() => this.setState({ indianAncestry: { XHRStatus: 'error' } }));
-  // };
 
   handleRacfid = event => {
     this.setState({ racfid: event.target.value });
@@ -81,15 +49,9 @@ export default class AddUser extends Component {
 
   onVerify = () => {
     const { email, racfid } = this.state;
-    this.props.actions.validateNewUserActions(email, racfid);
-    this.setState({ verify: true, addUser: true });
-  };
-
-  onSave = () => {
-    this.props.actions.createUserActions(this.state.verifyNewUserDetails.user);
-    this.setState({ add: true, addUser: false, verify: false });
-
     this.props.actions.fetchPermissionsActions();
+    this.props.actions.validateNewUserActions(email, racfid);
+    this.setState({ verify: true });
   };
 
   verifyUser = () => {
@@ -105,31 +67,20 @@ export default class AddUser extends Component {
     }
   };
 
-  fetchAddedUser() {
-    this.props.actions.fetchDetailsActions(this.state.createUserDetails);
-  }
-
-  addNewUSer = () => {
-    this.fetchAddedUser();
-    if (this.state.add) {
-      return (
-        <UserDetail
-          details={this.state.details}
-          permissionsList={this.state.permissionsList}
-        />
-      );
-    }
-  };
-
   addUser = () => {
     if (this.state.verify) {
       return (
         <AddVerifiedUser
           verifyNewUserDetails={this.state.verifyNewUserDetails}
-          onSave={this.onSave}
+          onSave={this.onAddUser}
         />
       );
     }
+  };
+
+  onAddUser = () => {
+    this.props.actions.createUserActions(this.state.verifyNewUserDetails.user);
+    this.setState({ addUser: false, verify: false });
   };
 
   render() {
@@ -139,8 +90,6 @@ export default class AddUser extends Component {
       userListClickHandler,
       dashboardClickHandler,
     } = this.props;
-    console.log('details', this.state.details);
-    console.log('details', this.state.createUserDetails);
     return this.state.addUser ? (
       <div>
         <PageHeader pageTitle="Add User" button="" />
@@ -164,7 +113,9 @@ export default class AddUser extends Component {
         </div>
       </div>
     ) : (
-      <div>{this.addNewUSer()}</div>
+      <div>
+        <AddUserDetail />
+      </div>
     );
   }
 }
@@ -176,10 +127,10 @@ AddUser.propTypes = {
   dashboardClickHandler: PropTypes.func,
   actions: PropTypes.object,
   verifyNewUserDetails: PropTypes.object,
-  createUserDetails: PropTypes.string,
-  details: PropTypes.object,
   addUser: PropTypes.bool,
-  permissionsList: PropTypes.array,
+  id: PropTypes.string,
+  permissionRoles: PropTypes.any,
+  details: PropTypes.any,
 };
 
 AddUser.defaultProps = {
