e08db32580a943179da4f5f2e6fa8a564d0311c0
Danny Eck <danny.eck@osi.ca.gov>
2018-08-01 20:38:31 -0700

    Fix rubocop lints

--- spec/acceptance/support/cognito_login_helper.rb
+++ spec/acceptance/support/cognito_login_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 module LoginHelper
-  def login()
+  def login
     visit ENV.fetch('RAILS_RELATIVE_URL_ROOT', '/')
-    return if not current_url.include?('amazoncognito.com')
+    return unless current_url.include?('amazoncognito.com')
     fill_in 'username', with: ENV.fetch('COGNITO_USERNAME', 'no-reply@osi.ca.gov')
     fill_in 'password', with: ENV.fetch('COGNITO_PASSWORD', 'password')
     submit_btn = find('input[type="Submit"]')
