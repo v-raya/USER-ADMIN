43c6dc4bc2a78f84d68aedaa4eafcddc09f7c2a9
nasir2712 <nasir.mohd2712@gmail.com>
2018-08-16 15:31:10 -0700

    COG-301 Clicking on Edit, CMS Login Disappears

--- app/javascript/views/userDetail/UserDetailEdit.jsx
+++ app/javascript/views/userDetail/UserDetailEdit.jsx
@@ -42,7 +42,7 @@ const UserDetailEdit = ({
               <ShowField label="Office Name">{details.office}</ShowField>
             </div>
             <div className="col-md-3">
-              <ShowField label="CWS Login">{details.RACFID}</ShowField>
+              <ShowField label="CWS Login">{details.racfid}</ShowField>
             </div>
             <div className="col-md-3">
               <ShowField label="Last Login">
