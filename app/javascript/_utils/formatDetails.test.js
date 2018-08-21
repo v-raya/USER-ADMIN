fc0942bdbcc5c0842047ad9c2028d6839b5b3cde
Ramu Kammagani <ramu.kammagani@osi.ca.gov>
2018-08-21 10:44:27 -0700

    COG-247 Add formatDate function to format the date

---
+++ app/javascript/_utils/formatDetails.test.js
@@ -0,0 +1,41 @@
+import { formatPhoneExtension, formatDate } from './formatDetails';
+
+describe('formatPhoneExtension', () => {
+  describe('When extension is available ', () => {
+    it('returns Extension with Ext text ', () => {
+      const details = {
+        phone_extension_number: '011',
+      };
+      expect(formatPhoneExtension(details)).toEqual(' Ext 011');
+    });
+  });
+
+  describe('When extension is not available ', () => {
+    it('returns just empty ', () => {
+      const details = {
+        phone_number: '1234567890',
+      };
+      expect(formatPhoneExtension(details)).toEqual('');
+    });
+  });
+});
+
+describe('#formatDate', () => {
+  describe('When date exists ', () => {
+    it('with different format', () => {
+      const details = {
+        start_date: '2001-09-01',
+      };
+      expect(formatDate(details.start_date)).toEqual('09/01/2001');
+    });
+  });
+
+  describe('When date not exists ', () => {
+    it('returns just empty ', () => {
+      const details = {
+        start_date: null,
+      };
+      expect(formatDate(details.start_date)).toEqual('');
+    });
+  });
+});
