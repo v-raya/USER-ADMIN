787df1bc481c522b058e25ede05695f0fb703cb3
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-08-22 17:08:06 -0700

    added constants from userlist and userdetails page

--- app/javascript/_constants/userDetailConstants.js
+++ app/javascript/_constants/userDetailConstants.js
@@ -1,4 +1,22 @@
+/* eslint camelcase: ["off"] */
+
+import { formatDate } from '../_utils/formatters';
+
 export const STATUS = [
   { value: 'Active', label: 'Active' },
   { value: 'Inactive', label: 'Inactive' },
 ];
+
+export const permissionListToOptions = items =>
+  items.map(({ name, description }) => ({ value: name, label: description }));
+
+export const toFullName = ({ first_name, last_name }) =>
+  `${last_name}, ${first_name}`;
+
+export const userStatusFormat = ({ enabled }) => {
+  return enabled ? 'Active' : 'Inactive';
+};
+
+export const lastLoginDate = ({ last_login_date_time }) => {
+  return formatDate(last_login_date_time);
+};
---
+++ app/javascript/_constants/userDetailConstants.test.js
@@ -0,0 +1,37 @@
+import {
+  toFullName,
+  userStatusFormat,
+  lastLoginDate,
+} from './userDetailConstants';
+
+describe('helpers', () => {
+  describe('toFullName', () => {
+    it('renders a full name', () => {
+      expect(toFullName({ first_name: 'First', last_name: 'Last' })).toEqual(
+        'Last, First'
+      );
+    });
+  });
+
+  describe('userStatusFormat', () => {
+    it('renders Active  for enabled', () => {
+      expect(userStatusFormat({ enabled: true })).toEqual('Active');
+    });
+
+    it('renders Activefor disabled', () => {
+      expect(userStatusFormat({ enabled: false })).toEqual('Inactive');
+    });
+  });
+
+  describe('#lastLoginDate', () => {
+    it('renders date in MM/DD/YYYY format', () => {
+      expect(lastLoginDate({ last_login_date_time: '2013-03-05' })).toEqual(
+        '03/05/2013'
+      );
+    });
+
+    it('renders empty when date does not exists', () => {
+      expect(lastLoginDate({ last_login_date_time: undefined })).toEqual('');
+    });
+  });
+});
