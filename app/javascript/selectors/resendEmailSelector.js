05679d99319e38b694e84340d7007c410558b16d
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-10-02 11:40:50 -0700

    disableResend button by comparing details's id and resendEmail id

--- app/javascript/selectors/resendEmailSelector.js
+++ app/javascript/selectors/resendEmailSelector.js
@@ -1,7 +1,27 @@
+import safeGet from 'lodash.get';
+
 export const resendEmail = state => {
   const resend = state.resendRegistrationEmail
     ? state.resendRegistrationEmail.resendEmailStatus
     : null;
   const statusMessage = resend === 200 ? 'Success' : 'Failure';
   return statusMessage;
+};
+
+export const resendId = state => {
+  const resendId = state.resendRegistrationEmail
+    ? state.resendRegistrationEmail.resendEmailId
+    : null;
+  return resendId;
+};
+
+export const disableResendEmailButton = state => {
+  const userDetailId = safeGet(state, 'fetchDetails.details.records.user.id');
+  const resendEmailId = resendId(state);
+  let index = -1;
+  if (resendEmailId && resendEmailId.length > 0 && userDetailId) {
+    index = resendEmailId.findIndex(id => id === userDetailId);
+  }
+  if (index > -1) return true;
+  return false;
 };
